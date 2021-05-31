7ac8289ae7e48d11d7db1056959b0d76c6fea6f0
Roman Kulyk <roman.kulyk@ucu.edu.ua>
2021-05-31 16:39:41 +0300

    update animation boxes

--- visualisation.py
+++ visualisation.py
@@ -5,12 +5,13 @@ from matplotlib import animation
 from person import Grid, Person
 from finite_state_machine import FiniteStateMachine
 from time import sleep
+from matplotlib.backend_bases import MouseButton
 
 # this is for pycharm
 # import matplotlib; matplotlib.use("TkAgg")
 
 FRAMES = 100
-INTERVAL = 100
+INTERVAL = 1
 # arrays_lst = [np.random.rand(100, 100) for _ in range(FRAMES)]
 
 
@@ -37,6 +38,18 @@ def animate(i):
     ax.set(xticklabels=[], yticklabels=[])
     ax.tick_params(bottom=False, left=False)
     ax.set_title(f"Smokers around the world. Year {i + 1}.")
+    ax.text(-12, 1.5, 'Non-smokers_low: ',
+            bbox={'facecolor': "#86ff6b", 'alpha': 1, 'pad': 10})
+    ax.text(-12, 4.5, 'Non-smokers_high: ',
+            bbox={'facecolor': "#ffd24d", 'alpha': 1, 'pad': 10})
+    ax.text(-12, 7.5, 'Junior smokers: ',
+            bbox={'facecolor': "#ffa46b", 'alpha': 1, 'pad': 10})
+    ax.text(-12, 10.5, 'Senior smokers: ',
+            bbox={'facecolor': "#ff6b6b", 'alpha': 1, 'pad': 10})
+    ax.text(-12, 13.5, 'Quit smoking: ',
+            bbox={'facecolor': "#b8b8b8", 'alpha': 1, 'pad': 10})
+    ax.text(-12, 16.5, 'Nobody: ',
+            bbox={'facecolor': "#ededed", 'alpha': 1, 'pad': 10})
 
 
 class PauseAnimation:
@@ -55,31 +68,12 @@ class PauseAnimation:
         c_bar.set_ticklabels(
             ['Nobody', 'Quit smoking', 'Senior smokers', 'Junior smokers', 'Non-smokers_high', 'Non-smokers_low'])
         plt.title("Smokers around the world.")
-        ax.text(-12, 1.5, 'Non-smokers_low: ',
-                bbox={'facecolor': "#86ff6b", 'alpha': 1, 'pad': 10})
-        ax.text(-12, 4.5, 'Non-smokers_high: ',
-                bbox={'facecolor': "#ffd24d", 'alpha': 1, 'pad': 10})
-        ax.text(-12, 7.5, 'Junior smokers: ',
-                bbox={'facecolor': "#ffa46b", 'alpha': 1, 'pad': 10})
-        ax.text(-12, 10.5, 'Senior smokers: ',
-                bbox={'facecolor': "#ff6b6b", 'alpha': 1, 'pad': 10})
-        ax.text(-12, 13.5, 'Quit smoking: ',
-                bbox={'facecolor': "#b8b8b8", 'alpha': 1, 'pad': 10})
-        ax.text(-12, 16.5, 'Nobody: ',
-                bbox={'facecolor': "#ededed", 'alpha': 1, 'pad': 10})
 
-        self.animation = animation.FuncAnimation(fig, animate, init_func=init, frames=FRAMES, repeat=True,
+        self.animation = animation.FuncAnimation(fig, animate, init_func=init, frames=FRAMES, repeat=False,
                                                  interval=INTERVAL)
         self.paused = False
 
         fig.canvas.mpl_connect('button_press_event', self.toggle_pause)
-
-    def toggle_pause(self, *args, **kwargs):
-        if self.paused:
-            self.animation.resume()
-        else:
-            self.animation.pause()
-        self.paused = not self.paused
 
 
 pa = PauseAnimation()
